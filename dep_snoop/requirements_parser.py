6de70111a16853fb93b2b53e8a8145c4bc9942df
Alexander "Django" Gschrei <alexander.gschrei@fau.de>
2020-07-24 21:25:32 +0000

    wip: added total ordering on version strings

--- dep_snoop/requirements_parser.py
+++ dep_snoop/requirements_parser.py
@@ -1,3 +1,4 @@
+import functools
 import re
 from typing import Callable
 
@@ -16,17 +17,56 @@ class Requirement:
         for requirements_group in version_requirements:
             for version_requirement in requirements_group.split(","):
                 print(version_requirement + "\n")
-        clauses = version_requirements.split(",")
-        return lambda x: x
 
     def _extract_predicate_helper(self, predicate_string: str):
         pass
 
 req = Requirement("urllib3 (!=1.25.1,<1.26,!=1.25.0,>=1.21.1)")
 
+
+@functools.total_ordering
 class VersionString:
     """ builds a hierarchical representation of a version from its string representation
-        we assume compliance with https://www.python.org/dev/peps/pep-0440/#version-scheme """
+        we assume compliance with https://www.python.org/dev/peps/pep-0440/#version-scheme 
+        This raises a ValueError if instantiated with an illegal verstion string"""
     def __init__(self, version: str):
-        regexp = r"([0-9]+!)?([0-9]+)((?:\.[0-9]+)*)((?:a|b|rc)[0-9]+)?(\.post[0-9]+)?(\.dev[0-9]+)?"
+        regexp = re.compile(r"(?P<epoch>[0-9]+!)?(?P<release>([0-9]+)((?:\.[0-9]+)*))"+
+        r"(?P<prerelease>(?:a|b|rc)[0-9]+)?(?P<post>\.post[0-9]+)?(?P<dev>\.dev[0-9]+)?")
+        matches = regexp.match(version)
+        if matches!=None:
+            groups = matches.groupdict()
+            self.epoch = groups.get("epoch")
+            self.release = groups.get("release")
+            self.prerelease = groups.get("prerelease")
+            self.post = groups.get("post")
+            self.dev = groups.get("dev")
+            print(groups)
+        else:
+            raise ValueError("The string you passed is not in compliance with PEP440!")
+    
+    def __lt__(self, other):
+        if not self.epoch < other.epoch:
+            if not self.release < other.release:
+                if other.prerelease == None or self.prerelease != None and not self.prerelease < other.prerelease:
+                    if not self.post < other.post:
+                        if not self.dev < other.dev:
+                            return False
+        return True
 
+    def __eq__(self, other):
+        return self.epoch == other.epoch and self.release == other.release and \
+        self.prerelease == other.prerelease and self.post == other.post and \
+        self.dev == other.dev
+
+class VersionStringBuilder:
+    @classmethod
+    def buildFromString(cls, version: str) -> VersionString:
+        try:
+            return VersionString(version)
+        except ValueError:
+            return None
+
+print(VersionStringBuilder.buildFromString("1!2.3.4rc5.post5.dev1"))
+a = VersionStringBuilder.buildFromString("1!2.3.4rc5.post5.dev1")
+b = VersionStringBuilder.buildFromString("1!2.3.post5.dev0")
+print(a < b)
